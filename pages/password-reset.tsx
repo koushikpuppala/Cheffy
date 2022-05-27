c388c7e0b3afd754626dffba6488faf142755a0f
koushikpuppala <koushikpuppala203@gmail.com>
2022-05-27 14:36:27 +0530

    Updated Password Reset

--- pages/password-reset.tsx
+++ pages/password-reset.tsx
@@ -8,52 +8,43 @@ const PasswordReset: NextPage = () => {
 		<>
 			<section id='hero' className={style.hero}>
 				<div className={`${style.container}`}>
-					<div className={`card text-center ${style.card}`}>
-						<div className={`card-header ${style.card_head}`}>
-							<div className={`${style.symbol}`}>
-								<span className={`${style.symbol_item}`}>
-									<i className='bi bi-shield-lock'></i>
-								</span>
-							</div>
-							<h1 className={`card-title ${style.card_head_title}`}>
-								Set a new Password
-							</h1>
-							Your new password must be different to previously used passwords.
+					<div className={style.symbol}>
+						<div className={style.left}></div>
+						<div className={style.line}></div>
+						<div className={style.center}>
+							<i className='bi bi-shield-lock'></i>
 						</div>
-						<div className={`card-body ${style.card_body}`}>
-							<form className='col-md-8'>
-								<div className='input-group col-12'>
-									<div className='input-group-text col-2'>
-										<i className='bi bi-shield-lock col-12'></i>
-									</div>
-									<div className='form-floating col-10'>
-										<input
-											type='password'
-											className='form-control'
-											id='password'
-											placeholder='Cheffy@123'
-											style={{
-												borderRadius: '0 0.375rem 0.375rem 0',
-											}}
-										/>
-										<label
-											htmlFor='password'
-											style={{
-												color: '#000',
-											}}>
-											Enter Password
-										</label>
-									</div>
-								</div>
-								<i className={style.reset_pass}>Reset Password</i>
-								<Link href='/'>
-									<a>
-										<i className='bi bi-arrow-left'></i> Back to Sign In
-									</a>
-								</Link>
-							</form>
-						</div>
+						<div className={style.right}></div>
 					</div>
+					<h1 className={`${style.card_title}`}>Set a New Password</h1>
+					<p className={style.card_text}>
+						Your new password must be different to previously used passwords.
+					</p>
+					<form className='col-md-10'>
+						<div className={`input-group col-12 ${style.form_group}`}>
+							<div className='input-group-text col-2'>
+								<i className='bi bi-shield-lock col-12'></i>
+							</div>
+							<div className='form-floating col-10'>
+								<input
+									type='password'
+									className='form-control'
+									id='password'
+									placeholder='Cheffy@123'
+									style={{
+										borderRadius: '0 0.375rem 0.375rem 0',
+									}}
+								/>
+								<label htmlFor='password'>Enter Password</label>
+							</div>
+						</div>
+						<button className={`${style.reset_pass} col-md-12`}>RESET PASSWORD</button>
+						<Link href='#'>
+							<a className={`bi bi-arrow-left ${style.back_sign_in}`}>
+								&nbsp;Back to Sign In
+							</a>
+						</Link>
+					</form>
 				</div>
 			</section>
 		</>
